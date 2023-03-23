8362a888d42c47a4f83e308d88d9baf131010b14
Neil Chadwick <neilchadders@hotmail.com>
2023-03-23 18:27:40 +0000

    add friends array

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const friends = [
     {
         id: 1,
         name: 'Sir Isaac Newton',
-    }
+    },
     {
         id: 2,
         name: 'Albert Einstein',
     }
 ]
 
-server.on('request', (req, res) => {
+server.on('request', (req, res) => { // These are the two streams node makes available for us
     const items = req.url.split('/'); //remove / and create an array
 
     if (req.method === 'POST' && items[1] === 'friends') {
@@ -29,8 +29,32 @@ server.on('request', (req, res) => {
             friends.push(JSON.parse(friend));
         });
         req.pipe(res);
+    } else if (req.method === 'GET' && items[1] === 'friends') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (req.method === 'GET' && items[1] === 'messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello Isaac!</li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
     }
+});
 
-    server.listen(PORT, () => {
-        console.log(`listening on port ${PORT}....`)
-    });
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
