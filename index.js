f7c557a5c18d49a7678367286b654ec3c4045830
Neil Chadwick <neilchadders@hotmail.com>
2023-03-23 18:10:08 +0000

    add friends array

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',//responds with JSON
-    });
-    res.end(JSON.stringify({
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 0,
+        name: 'Nikolai Telsa',
+    },
+    {
         id: 1,
         name: 'Sir Isaac Newton',
-    }));
-});
+    }
+    {
+        id: 2,
+        name: 'Albert Einstein',
+    }
+]
 
-server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}....`)
-});
+server.on('request', (req, res) => {
+    const items = req.url.split('/'); //remove / and create an array
+
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friends.push(JSON.parse(friend));
+        });
+        req.pipe(res);
+    }
+
+    server.listen(PORT, () => {
+        console.log(`listening on port ${PORT}....`)
+    });
